c8eed4f6d588ffffdeba6e3dc38798745909fe5f
Manuel Heredia <manolo.heredia@gmail.com>
2022-08-17 17:52:18 +0200

    ROS Course 1 Asignment 7 Bumper robot - 1st working version

--- src/mhered/src/bumper_app.py
+++ src/mhered/src/bumper_app.py
@@ -3,73 +3,95 @@
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import euler_from_quaternion
+import numpy as np
+from sensor_msgs.msg import LaserScan
 
 
 import math
 import time
 
-from std_srvs.srv import Empty
-
+# use current location from the global variables
+# (constantly updated by odom_callback())
+global x, y, yaw, fwd_clearance
 x = 0.0
 y = 0.0
 yaw = 0.0
+fwd_clearance = 0.0
+
+# get global params
+global LASER_RANGE, BEAM_ANGLE
+global ANGLE_TOL, SAFETY_DIST, MIN_CLEARANCE
+global WAIT, LIN_SPEED, ROT_SPEED, RATE
 
 
 def odom_callback(odom_message):
-    """ Pose callback method """
+    """
+    Constantly extract robot pose from /odom nav_msgs/Odometry
+    """
+
+    # update global variables
     global x, y, yaw
+
     x = odom_message.pose.pose.position.x
     y = odom_message.pose.pose.position.y
     q = odom_message.pose.pose.orientation
     q_as_list = [q.x, q.y, q.z, q.w]
-    (roll, pitch, yaw) = euler_from_quaternion(q_as_list)
+    (_, _, yaw) = euler_from_quaternion(q_as_list)
 
+
+def scan_callback(message):
     """
-    /odom nav_msgs/Odometry
-    header:
-        seq: 0
-        stamp:
-            secs: 0
-            nsecs: 0
-        frame_id: ''
-        child_frame_id: ''
-    pose:
-        pose:
-            position: {x: 0.0, y: 0.0, z: 0.0}
-            orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}
-        covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    twist:
-        twist:
-            linear: {x: 0.0, y: 0.0, z: 0.0}
-            angular: {x: 0.0, y: 0.0, z: 0.0}
-        covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
+    Constantly update global fwd_clearance from /scan sensor_msgs/LaserScan
     """
 
+    global fwd_clearance
 
-def move(velocity_publisher, speed, distance, is_forward):
+    # parameters
+    global BEAM_ANGLE, LASER_RANGE
+
+    ranges = np.asarray(message.ranges)
+    angles = np.degrees((message.angle_min +
+                         message.angle_increment * np.asarray(
+                             range(len(ranges)))))
+
+    # shift angles >180 from [180 - 360) to [-180, 0)
+    angles_shifted = np.where(angles > 180, angles-360, angles)
+
+    angles_indeces = angles_shifted.argsort()
+    angles_sorted = angles_shifted[angles_indeces[::-1]]
+    ranges_sorted = ranges[angles_indeces[::-1]]
+
+    # remove Inf and NaN values from ydata to compute axes limits
+    # deep copy to avoid modifying ydata
+    clean_ranges_sorted = ranges_sorted.copy()
+    clean_ranges_sorted[~np.isfinite(clean_ranges_sorted)] = LASER_RANGE
+
+    # slice the beam at -BEAM_ANGLE +BEAM_ANGLE
+    fwd_ranges = [r for (a, r) in zip(
+        angles_sorted, clean_ranges_sorted) if abs(a) < BEAM_ANGLE]
+    fwd_clearance = min(fwd_ranges)
+    print(f"FWD CLEARANCE:{fwd_clearance:8.4}")
+
+
+def move_fwd(velocity_publisher, speed):
     """ Straight motion method """
 
     # declare a Twist message to send velocity commands
     velocity_message = Twist()
 
     # get current location from the global variable before entering the loop
-    global x, y
+    global x, y, fwd_clearance
+    global SAFETY_DIST, RATE
     # save initial coordinates
     x0 = x
     y0 = y
 
-    if (is_forward):
-        velocity_message.linear.x = abs(speed)
-    else:
-        velocity_message.linear.x = -abs(speed)
+    velocity_message.linear.x = speed
 
     distance_moved = 0.0
-    loop_rate = rospy.Rate(50)  # we publish the velocity at 10 Hz (10 times per second)
+    # we publish the velocity at RATE Hz (RATE times per second)
+    loop_rate = rospy.Rate(RATE)
 
     rospy.loginfo("Straight motion")
     while True:
@@ -79,31 +101,29 @@ def move(velocity_publisher, speed, distance, is_forward):
 
         distance_moved = abs(math.sqrt(((x-x0) ** 2) + ((y-y0) ** 2)))
         print(f"Distance moved: {distance_moved:10.4} Pose: ({x:8.4}, {y:8.4}, {yaw:8.4})")
-        if not (distance_moved < distance):
-            rospy.loginfo("Distance reached")
+        if fwd_clearance < SAFETY_DIST:
+            rospy.loginfo("Obstacle reached")
             break
 
-    # stop the robot after the distance is reached
+    rospy.loginfo("Stopping")
+    # stop the robot when obstacle is reached
     velocity_message.linear.x = 0.0
     velocity_publisher.publish(velocity_message)
 
 
-def rotate(velocity_publisher, omega_degrees, angle_degrees, is_clockwise):
+def rotate_in_place(velocity_publisher, omega_degrees):
     """ Rotation in place method """
 
+    global RATE, MIN_CLEARANCE
     # declare a Twist message to send velocity commands
     velocity_message = Twist()
 
     omega = math.radians(omega_degrees)
 
-    if (is_clockwise):
-        velocity_message.angular.z = -abs(omega)
-    else:
-        velocity_message.angular.z = abs(omega)
-
-    loop_rate = rospy.Rate(50)  # we publish the velocity at 10 Hz (10 times per second)
-
-    rospy.loginfo("Rotation in place")
+    # publish velocity message to rotate
+    velocity_message.angular.z = omega
+    # at RATE Hz (RATE times per second)
+    loop_rate = rospy.Rate(RATE)
 
     # get initial timestamp
     t0 = rospy.Time.now().to_sec()
@@ -118,142 +138,40 @@ def rotate(velocity_publisher, omega_degrees, angle_degrees, is_clockwise):
         loop_rate.sleep()
 
         print(f"Angle rotated: {curr_yaw_degrees:10.4} Pose: ({x:8.4}, {y:8.4}, {yaw:8.4})")
-        if not(curr_yaw_degrees < angle_degrees):
+        if fwd_clearance > MIN_CLEARANCE:
             rospy.loginfo("Angle reached")
             break
 
     # stop the robot after the angle is reached
     velocity_message.angular.z = 0.0
     velocity_publisher.publish(velocity_message)
+    rospy.loginfo("Stopping rotation")
 
 
-def set_yaw(velocity_publisher, orientation_degrees):
-    """ Set absolute orientation method """
-
-    # declare a Twist message to send velocity commands
-    velocity_message = Twist()
-
-    # get current location from the global variable before entering the loop
-    global yaw
-
-    yaw_degrees = math.degrees(yaw)
-
-    # subtract angles, wrapping result to [-180, 180]
-    angle_degrees = ((orientation_degrees - yaw_degrees + 180) % 360) - 180
-
-    # rotate towards smallest angle difference
-    if angle_degrees < 0:
-        is_clockwise = True
-    else:
-        is_clockwise = False
-
-    rotate(velocity_publisher, 15, abs(angle_degrees), is_clockwise)
-    rospy.loginfo(f"Orientation set to {orientation_degrees}")
-
-
-def go_to(velocity_publisher, goal):
-    """ Go to goal method """
-
-    # declare a Twist message to send velocity commands
-    velocity_message = Twist()
-
-    # use current location from the global variable (constantly updated by pose_callback())
-    global x, y, yaw
-
-    x_goal = goal[0]
-    y_goal = goal[1]
-
-    THRESHOLD = 0.1
-    K_DISTANCE = .6
-    K_ANGLE = 15
-
-    loop_rate = rospy.Rate(50)  # we publish the velocity at 10 Hz (10 times per second)
-
-    rospy.loginfo(f"Go to goal: {goal}")
-
-    while True:
-
-        distance_to_goal = abs(math.sqrt(((x_goal-x) ** 2) + ((y_goal-y) ** 2)))
-        angle_to_goal = math.atan2(y_goal-y, x_goal-x)
-
-        print(f"Distance to goal: {distance_to_goal:10.4} Pose: ({x:8.4}, {y:8.4}, {yaw:8.4})")
-
-        if distance_to_goal < THRESHOLD:
-            rospy.loginfo("Goal reached")
-            break
-
-        velocity_message.linear.x = K_DISTANCE * distance_to_goal
-        velocity_message.angular.z = K_ANGLE * (angle_to_goal - yaw)
-
-        velocity_publisher.publish(velocity_message)
-        loop_rate.sleep()
-
-    # stop the robot after the distance is reached
-    velocity_message.linear.x = 0.0
-    velocity_message.angular.z = 0.0
-    velocity_publisher.publish(velocity_message)
-
-
-def spiral(velocity_publisher, omega, d_vel):
-    """ Spiral method """
-
-    # use current location from the global variable (constantly updated by pose_callback())
-    global x, y
-
-    # declare a Twist message to send velocity commands
-    velocity_message = Twist()
-
-    OMEGA = omega
-    VEL_INCREMENT = d_vel
-
-    loop_rate = rospy.Rate(50)  # we publish the velocity at 10 Hz (10 times per second)
-
-    rospy.loginfo("Spiral")
-
-    while x > 0.5 and x < 10.5 and y > 0.5 and y < 10.5:
-
-        velocity_message.linear.x += VEL_INCREMENT
-        velocity_message.angular.z = OMEGA
-
-        print(f"Linear speed: {velocity_message.linear.x:10.4} Pose: ({x:8.4}, {y:8.4}, {yaw:8.4})")
-
-        velocity_publisher.publish(velocity_message)
-        loop_rate.sleep()
-
-    rospy.loginfo("Edge reached")
-
-    # stop the robot after condition is fulfilled
-    velocity_message.linear.x = 0.0
-    velocity_message.angular.z = 0.0
-    velocity_publisher.publish(velocity_message)
-
-
-def bumper_app(velocity_publisher, WAIT, LIN_SPEED, ROT_SPEED):
+def bumper_app(velocity_publisher):
     """ Bumper robot """
 
-    # save initial position
-    global x, y, yaw
-    x0, y0, yaw0 = x, y, yaw
+    # use current location from the global variables
+    # (constantly updated by odom_callback())
+    global x, y, yaw, fwd_clearance
 
-    go_to(velocity_publisher=velocity_publisher,
-          goal=(3, 3))
-    time.sleep(WAIT)
+    # get global params
+    global LASER_RANGE, BEAM_ANGLE
+    global ANGLE_TOL, SAFETY_DIST, MIN_CLEARANCE
+    global WAIT, LIN_SPEED, ROT_SPEED, RATE
 
-    set_yaw(velocity_publisher=velocity_publisher,
-            orientation_degrees=90)
-    time.sleep(WAIT)
+    while True:
+        # Behavior 1: move fwd until blocked by obstacle
+        move_fwd(velocity_publisher=velocity_publisher,
+                 speed=LIN_SPEED)
+        print("** Reached obstacle\n\n")
+        time.sleep(WAIT)
 
-    move(velocity_publisher=velocity_publisher,
-         speed=LIN_SPEED,
-         distance=10.0,
-         is_forward=True)
-    time.sleep(WAIT)
-
-    rotate(velocity_publisher=velocity_publisher,
-           omega_degrees=ROT_SPEED,
-           angle_degrees=90,
-           is_clockwise=True)
-    time.sleep(WAIT)
+        # Behavior 2: rotate until clearance found
+        rotate_in_place(velocity_publisher=velocity_publisher,
+                        omega_degrees=ROT_SPEED)
+        print("** Found direction clear of obstacles\n\n")
+        time.sleep(WAIT)
 
 
 if __name__ == '__main__':
@@ -266,27 +184,37 @@ if __name__ == '__main__':
         cmd_vel_topic = '/cmd_vel'
         velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
 
-        # declare odom subscriber
+        # declare /odom subscriber
         odom_topic = "/odom"
         odom_subscriber = rospy.Subscriber(odom_topic, Odometry, odom_callback)
 
-        time.sleep(1)
+        # declare /scan subscriber
+        scan_topic = "/scan"
+        scan_subscriber = rospy.Subscriber(scan_topic, LaserScan, scan_callback)
+
+        time.sleep(2.0)
 
         # get ROS parameters (or default)
-        WAIT = rospy.get_param("WAIT", 0.5)
-        LIN_SPEED = rospy.get_param("LIN_SPEED", 4.0)
-        ROT_SPEED = rospy.get_param("ROT_SPEED", 30.0)
+        global LASER_RANGE, BEAM_ANGLE
+        global ANGLE_TOL, SAFETY_DIST, MIN_CLEARANCE
+        global WAIT, LIN_SPEED, ROT_SPEED, RATE
 
-        # call the function
-        bumper_app(velocity_publisher, WAIT, LIN_SPEED, ROT_SPEED)
+        LASER_RANGE = rospy.get_param("LASER_RANGE", 5.0)  # m
+        BEAM_ANGLE = rospy.get_param("BEAM_ANGLE", 5.0)  # degrees
 
-        print('start reset: ')
-        time.sleep(5)
-        rospy.wait_for_service('reset')
-        reset_turtle = rospy.ServiceProxy('reset', Empty)
-        reset_turtle()
-        print('end reset: ')
+        ANGLE_TOL = rospy.get_param("ANGLE_TOL", 1.0)  # degrees
+        SAFETY_DIST = rospy.get_param("SAFETY_DIST", 0.6)  # m
+        MIN_CLEARANCE = rospy.get_param("MIN_CLEARANCE", 3.0)  # m
+
+        WAIT = rospy.get_param("WAIT", .5)  # s
+        LIN_SPEED = rospy.get_param("LIN_SPEED", 0.4)  # m/s
+        ROT_SPEED = rospy.get_param("ROT_SPEED", 15.0)  # degrees/s
+        RATE = rospy.get_param("RATE", 10.0)  # Hz
+
+        # call the bumper app
+        bumper_app(velocity_publisher)
 
         rospy.spin()
+
     except rospy.ROSInterruptException:
-        rospy.loginfo("node terminated.")
+        rospy.loginfo("Node terminated")
